8ce74e630271fa7c933ee8fa9185004b51c67d27
Isaac23-K <ikaptoge@yahoo.com>
2021-07-05 14:19:39 +0300

    modified array adapters

--- app/src/main/java/com/moringaschool/eathelathy/RecipeArrayAdapter.java
+++ app/src/main/java/com/moringaschool/eathelathy/RecipeArrayAdapter.java
@@ -1,4 +1,26 @@
-package com.moringaschool.eathelathy;
+package com.moringaschool.eathealthy;
 
-public class RecipeArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class RecipeArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRecipes;
+
+
+    public RecipeArrayAdapter(Context mContext, int resource, String[] mRecipes) {
+        super(mContext, resource);
+        this.mContext = mContext ;
+        this.mRecipes = mRecipes ;
+
+    }
+    @Override
+    public Object getItem (int position ){
+        String recipes = mRecipes [position];
+        return String.format("%s \nServes great: %s",recipes);
+    }
+    @Override
+    public int getCount(){
+        return mRecipes.length;
+    }
 }

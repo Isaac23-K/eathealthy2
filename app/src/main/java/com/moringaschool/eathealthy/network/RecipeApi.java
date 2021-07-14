44f94ada07c98abae7d7490a062bd84b010c949f
Isaac23-K <ikaptoge@yahoo.com>
2021-07-14 23:18:18 +0300

    changed internet access

--- app/src/main/java/com/moringaschool/eathealthy/network/RecipeApi.java
+++ app/src/main/java/com/moringaschool/eathealthy/network/RecipeApi.java
@@ -1,4 +1,15 @@
 package com.moringaschool.eathealthy.network;
 
+import com.moringaschool.eathealthy.models.RecipeSearch;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+    @GET("search")
+    Call<RecipeSearch> getRecipe(
+            @Query("q") String q
+    );
+
 }
--- app/src/main/java/com/moringaschool/eathealthy/network/RecipeClient.java
+++ app/src/main/java/com/moringaschool/eathealthy/network/RecipeClient.java
@@ -1,4 +1,39 @@
 package com.moringaschool.eathealthy.network;
 
+import com.moringaschool.eathealthy.Constants;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+
 public class RecipeClient {
+    private static Retrofit retrofit = null;
+
+    public static RecipeApi getClient() {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request newRequest = chain.request().newBuilder()
+                            .build();
+                    return chain.proceed(newRequest);
+                }
+            })
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(EDAMAM_BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(RecipeApi.class);
+    }
 }
